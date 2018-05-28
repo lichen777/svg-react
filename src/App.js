fc468e7e9ee68a90e178bcd2f581cb11a065f865
LICHEN <lchen139@gmail.com>
2018-05-28 11:40:20 -0500

    add color control

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import Avatar from './components/avatar';
+import hairExample from "./elements/male-hair-1-40.svg"
+import faceExample from "./elements/basic-face-2-110.svg"
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Avatar 
-          width="200px"
-          hair="http://res.cloudinary.com/lchen139/image/upload/v1527436345/tulip_bag.svg"
+          width = "200px"
+          haircolor = "#2b5fa7"
+          skincolor = "#2b5fa7"
+          face={faceExample}
+          hair={hairExample}
         />
       </div>
     );
--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import SVG from "react-inlinesvg";
 
-export default function Avatar (props) {
-    const { body, face, cloth, mouth, nose, eyes, eyebrow, hair, glass, accessory } = props;
-        return <svg viewBox="0 0 20 20" {...props}>
-            <SVG src={body} wrapper={React.createFactory("svg")} />
-            <SVG src={face} wrapper={React.createFactory("svg")} />
-            <SVG src={cloth} wrapper={React.createFactory("svg")} />
-            <SVG src={mouth} wrapper={React.createFactory("svg")} />
-            <SVG src={nose} wrapper={React.createFactory("svg")} />
-            <SVG src={eyes} wrapper={React.createFactory("svg")} />
-            <SVG src={eyebrow} wrapper={React.createFactory("svg")} />
-            <SVG src={hair} wrapper={React.createFactory("svg")} />
-            <SVG src={glass} wrapper={React.createFactory("svg")} />
-            <SVG src={accessory} wrapper={React.createFactory("svg")} />
-          </svg>;
+export default function Avatar(props) {
+    const { body, face, skincolor, cloth, mouth, nose, eyes, eyebrow, hair, haircolor, glass, accessory } = props;
+    return <svg viewBox="0 0 20 20" {...props}>
+        <SVG src={body} style={{ fill: skincolor }} wrapper={React.createFactory("svg")} />
+        <SVG src={face} style={{ fill: skincolor }} wrapper={React.createFactory("svg")} />
+        <SVG src={cloth} wrapper={React.createFactory("svg")} />
+        <SVG src={mouth} wrapper={React.createFactory("svg")} />
+        <SVG src={nose} wrapper={React.createFactory("svg")} />
+        <SVG src={eyes} wrapper={React.createFactory("svg")} />
+        <SVG src={eyebrow} wrapper={React.createFactory("svg")} />
+        <SVG src={hair} style={{ fill: haircolor }} wrapper={React.createFactory("svg")} />
+        <SVG src={glass} wrapper={React.createFactory("svg")} />
+        <SVG src={accessory} wrapper={React.createFactory("svg")} />
+      </svg>;
 }
 
